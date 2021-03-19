81b657e68782c85567136d5876f0af0e9e5b172d
Nikita Stepanov <prizrachnij@gmail.com>
2021-03-20 00:39:19 +0300

    create: paralax effect in newsletter section

--- js/main.js
+++ js/main.js
@@ -26,3 +26,15 @@ var reviewSlider = new Swiper('.reviews-slider', {
       onlyInViewport: false,
     },
 });
+
+$(document).ready(function(){
+  $window = $(window);
+$('div[data-type="background"]').each(function(){
+   var $bgobj = $(this);
+$(window).scroll(function() {
+      var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+      var coords = '50% '+ yPos + 'px';
+      $bgobj.css({ backgroundPosition: coords });
+  }); 
+});    
+}); 
